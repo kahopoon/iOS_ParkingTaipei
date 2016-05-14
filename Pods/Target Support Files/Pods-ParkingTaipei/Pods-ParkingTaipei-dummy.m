75c5b4b513376fa7475c55402ef1298bb2d28026
Johnson Kaho Poon <kahopoon@gmail.com>
2016-05-14 21:21:41 +0800

    pod installed

---
+++ Pods/Target Support Files/Pods-ParkingTaipei/Pods-ParkingTaipei-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParkingTaipei : NSObject
+@end
+@implementation PodsDummy_Pods_ParkingTaipei
+@end
---
+++ Pods/Target Support Files/Pods-ParkingTaipei/Pods-ParkingTaipei-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ParkingTaipeiVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ParkingTaipeiVersionString[];
+
